8c71a7429453ba6dcaa6bae951c5532f4d902397
Julián Zapata Vidal <julianzapatavidal@gmail.com>
2023-11-23 16:32:04 -0300

    proyecto de CRUD con Express y React, se vincula con bdd, faltan hacer otros módulos

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-
-
 const db = mysql.createConnection({
     host:"127.0.0.1",
     user:"chu",
@@ -15,50 +13,27 @@ const db = mysql.createConnection({
     database:"proyecto1",
 });
 
-app.get("/muestra", (req, res)=>{
-    db.query('SELECT * FROM detalle',(err, result)=>{
-        if(err){
-            console.log (err);
-        }else{
-            res.send(result);
-        }
-    });
+app.listen(3131, ()=>{
+    console.log("Ejecutando backend en puerto 3131");
 });
 
+app.get("/muestra", (req, res)=>{
+    db.query('SELECT * FROM detalle',(err, result)=>{
+        if(err){console.log (err);}else{res.send(result);}});});
+
 app.post("/inserta",(req,res)=>{
-    /*const articulo = req.body.articulo;
-    const detalle = req.body.detalle;
-    const observaciones = req.body.observaciones;
-    
-    const qInsert = 'INSERT INTO detalle (articulo, detalle, observaciones) VALUES (?, ? ,?)'
-    db.query(qInsert, [articulo, detalle, observaciones],
-        (err,result)=>{
-            if(err){
-                console.log(err);
-            }else{
-                res.send("alumno registrado");
-            }
-        });*/
-    req.map((x) => {
-        const articulo = x.body.articulo;
-        const cantidad = x.body.cantidad;
-        const observaciones = x.body.observaciones;
-    
+        req.body.map((x) => {
+        const articulo = x.articulo;
+        const cantidad = x.cantidad;
+        const observaciones = x.observaciones;
         const qInsert = 'INSERT INTO detalle (articulo, cantidad, observaciones) VALUES (?, ? ,?)'
         db.query(qInsert, [articulo, cantidad, observaciones],
             (err,result)=>{
                 if(err){
                     console.log(err);
                 }else{
-                    res.send("alumno registrado");
-                }
-            });
-    }
-        
-    )
+                    //res.send("alumno registrado");
+}});})});
 
-});
 
-app.listen(3131, ()=>{
-    console.log("Ejecutando backend en puerto 3131");
-});
+
